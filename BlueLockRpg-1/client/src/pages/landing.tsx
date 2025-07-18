4307d22fa7b3936c39d72d54dd081e0fe5bedc5b
DASILVA8282 <dasilvatalvez@gmail.com>
2025-07-18 19:38:50 -0300

    Update landing.tsx

--- BlueLockRpg-1/client/src/pages/landing.tsx
+++ BlueLockRpg-1/client/src/pages/landing.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
-import InvitationEnvelope from "@/components/ui/InvitationEnvelope";
+import InvitationEnvelope from "@/components/InvitationEnvelope";
 
 export default function Landing() {
   const { login, register, isLoginPending, isRegisterPending } = useAuth();
